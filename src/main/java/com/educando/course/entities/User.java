12f1eafbe15ad6411e321be89ae2809893203b1d
MaryJackS <silvajack470@gmail.com>
2025-07-17 10:09:44 -0300

    Order, Instant, iso 8601 - part 1

--- src/main/java/com/educando/course/entities/User.java
+++ src/main/java/com/educando/course/entities/User.java
@@ -1,12 +1,14 @@
 package com.educando.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -74,10 +79,17 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result +((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -89,9 +101,12 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if(id == null) {
+			if(other.id != null)
+				return false;
+		}else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 
 }
