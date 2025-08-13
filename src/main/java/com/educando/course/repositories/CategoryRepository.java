109d68f30eba233130af3ca1e152a54e134cc474
MaryJackS <silvajack470@gmail.com>
2025-08-13 10:17:27 -0300

    Category

---
+++ src/main/java/com/educando/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.educando.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educando.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+	
+}
